ce165af17c1dfe827a5d43b0c98fde09985d1950
Christo790 <126384113+Christo790@users.noreply.github.com>
2023-07-08 12:55:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,37 @@
 const express = require('express');
 const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-app.post('/send-message', (req, res) => {
-  const { message } = req.body;
-
-  // Handle the message (e.g., send it to the recipient(s) using a suitable method)
-  
-  // Respond with a success message
-  res.json({ message: 'Message sent successfully' });
-});
-
 app.use(express.static('public'));
 
-app.listen(port, () => {
+// Store messages in an array
+const messages = [];
+
+// Socket.io event handling
+io.on('connection', socket => {
+  console.log('A user connected');
+
+  // Send all existing messages to the client
+  socket.emit('initialMessages', messages);
+
+  // Handle new messages from clients
+  socket.on('newMessage', message => {
+    // Store the new message
+    messages.push(message);
+
+    // Broadcast the new message to all connected clients
+    io.emit('broadcastMessage', message);
+  });
+
+  // Handle disconnections
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+http.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
